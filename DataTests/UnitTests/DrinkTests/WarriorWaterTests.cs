547b970d362f3a28b1b4a409a025df7ce980af3d
Chlorobastion <neal01@ksu.edu>
2020-09-02 16:46:50 -0500

    Added WarriorWaterTest.cs and the corresponding documentation and testing therein. I believe Milestone #2 should be completed wholly now. (9/2/20)

---
+++ DataTests/UnitTests/DrinkTests/WarriorWaterTests.cs
@@ -0,0 +1,158 @@
+﻿/*
+ * Author: Sebastian Neal
+ * Class: WarriorWaterTests.cs
+ * Purpose: Test the WarriorWater.cs class in the Data library
+ */
+using Xunit;
+
+using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Enums;
+
+namespace BleakwindBuffet.DataTests.UnitTests.DrinkTests
+{
+    /// <summary>
+    /// Tests for the Warrior Water class.
+    /// </summary>
+    public class WarriorWaterTests
+    {
+        /// <summary>
+        /// Tests if ice is included by default (it should be).
+        /// </summary>
+        [Fact]
+        public void ShouldIncludeIceByDefault()
+        {
+            WarriorWater ww = new WarriorWater();
+            Assert.True(ww.Ice);
+        }
+
+        /// <summary>
+        /// Tests if lemon is included by default (it should not be).
+        /// </summary>
+        [Fact]
+        public void ShouldNotIncludeLemonByDefault()
+        {
+            WarriorWater ww = new WarriorWater();
+            Assert.False(ww.Lemon);
+        }
+
+        /// <summary>
+        /// Tests if size is initially small (it should be).
+        /// </summary>
+        [Fact]
+        public void ShouldBeSmallByDefault()
+        {
+            WarriorWater ww = new WarriorWater();
+            Assert.Equal(Size.Small, ww.Size);
+        }
+
+        /// <summary>
+        /// Tests if ice is able to be set.
+        /// </summary>
+        [Fact]
+        public void ShouldBeAbleToSetIce()
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Ice = false;
+            Assert.False(ww.Ice);
+            ww.Ice = true;
+            Assert.True(ww.Ice);
+        }
+
+        /// <summary>
+        /// Tests if lemon is able to be set.
+        /// </summary>
+        [Fact]
+        public void ShouldBeAbleToSetLemon()
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Lemon = true;
+            Assert.True(ww.Lemon);
+            ww.Lemon = false;
+            Assert.False(ww.Lemon);
+        }
+
+        /// <summary>
+        /// Tests if drink size is able to be set.
+        /// </summary>
+        [Fact]
+        public void ShouldBeAbleToSetSize()
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Size = Size.Large;
+            Assert.Equal(Size.Large, ww.Size);
+            ww.Size = Size.Medium;
+            Assert.Equal(Size.Medium, ww.Size);
+            ww.Size = Size.Small;
+            Assert.Equal(Size.Small, ww.Size);
+        }
+
+        /// <summary>
+        /// Tests if drink size is linked to drink price properly.
+        /// </summary>
+        /// <param name="size">The size of the drink (Small, Medium, or Large).</param>
+        /// <param name="price">The correct price of the particular drink and size.</param>
+        [Theory]
+        [InlineData(Size.Small, 0.00)]
+        [InlineData(Size.Medium, 0.00)]
+        [InlineData(Size.Large, 0.00)]
+        public void ShouldHaveCorrectPriceForSize(Size size, double price)
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Size = size;
+            Assert.Equal(price, ww.Price);
+        }
+
+        /// <summary>
+        /// Tests if drink size is linked to drink calories properly.
+        /// </summary>
+        /// <param name="size">The size of the drink (Small, Medium, or Large).</param>
+        /// <param name="cal">The correct calories of the particular drink and size.</param>
+        [Theory]
+        [InlineData(Size.Small, 0)]
+        [InlineData(Size.Medium, 0)]
+        [InlineData(Size.Large, 0)]
+        public void ShouldHaveCorrectCaloriesForSize(Size size, uint cal)
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Size = size;
+            Assert.Equal(cal, ww.Calories);
+        }
+
+        /// <summary>
+        /// Tests if special instructions work as desired.
+        /// </summary>
+        /// <param name="includeIce">Whether or not ice is to be included.</param>
+        /// <param name="includeLemon">Whether or not lemon is to be included.</param>
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(false, false)]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        public void ShouldHaveCorrectSpecialInstructions(bool includeIce, bool includeLemon)
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Ice = includeIce;
+            ww.Lemon = includeLemon;
+            if (!includeIce) { Assert.Contains("Hold ice", ww.SpecialInstructions); }
+            if (includeLemon) { Assert.Contains("Add lemon", ww.SpecialInstructions); }
+            if(includeIce && !includeLemon) { Assert.Empty(ww.SpecialInstructions); }
+        }
+
+        /// <summary>
+        /// Tests if the ToString() method returns the proper values.
+        /// </summary>
+        /// <param name="size">The size of the drink (Small, Medium, Large).</param>
+        /// <param name="name">The correct name of the drink.</param>
+        [Theory]
+        [InlineData(Size.Small, "Small Warrior Water")]
+        [InlineData(Size.Medium, "Medium Warrior Water")]
+        [InlineData(Size.Large, "Large Warrior Water")]
+        public void ShouldReturnCorrectToStringBasedOnSize(Size size, string name)
+        {
+            WarriorWater ww = new WarriorWater();
+            ww.Size = size;
+            Assert.Equal(name, ww.ToString());
+        }
+    }
+}
